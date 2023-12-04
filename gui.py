be79b24f8ad0440b10434493195d7b9fa94c1a59
Cheez-them <138711762+Cheez-them@users.noreply.github.com>
2023-12-04 08:59:07 +0530

    commit 2
    1. added word support only for 1
    2. more GUI updates like title and interactive buttons

--- gui.py
+++ gui.py
@@ -21,6 +21,8 @@ for i in range(26):
 
 # fonts
 LETTER_FONT = pygame.font.SysFont('comicsans', 25)
+WORD_FONT = pygame.font.SysFont('comicsans', 40)
+TITLE_FONT = pygame.font.SysFont('comicsans', 50)
 
 # Loading images
 images = []
@@ -29,7 +31,9 @@ for i in range(7):
     images.append(image)
 
 # Game variables
-hangman_status = 6
+hangman_status = 0
+word = "DEVELOPER"
+guessed = []
 
 # colors
 WHITE = (255, 255, 255)
@@ -44,6 +48,20 @@ run = True
 def draw():
     win.fill(WHITE)
 
+    # draw title
+    text = TITLE_FONT.render("HANGMAN", 1, BLACK)
+    win.blit(text, ((WIDTH - text.get_width()) / 2, 10))
+
+    # draw word
+    display_word = ""
+    for letter in word:
+        if letter in guessed:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    text = WORD_FONT.render(display_word, 1, BLACK)
+    win.blit(text, (400, 200))
+
     # draw buttons
     for letter in letters:
         x, y, ltr, visible = letter
@@ -52,8 +70,19 @@ def draw():
             text = LETTER_FONT.render(ltr, 1, BLACK)
             win.blit(text, (x - text.get_width() / 2, y - text.get_height() / 2))
 
+    # draw hangman and update display
     win.blit(images[hangman_status], (150, 100))
     pygame.display.update()
+
+
+# win or lose
+def display_message(message):
+    pygame.time.delay(1000)
+    win.fill(WHITE)
+    text = WORD_FONT.render(message, 1, BLACK)
+    win.blit(text, ((WIDTH - text.get_width()) / 2, (HEIGHT - text.get_height()) / 2))
+    pygame.display.update()
+    pygame.time.delay(3000)
 
 
 # Game loop
@@ -73,8 +102,27 @@ while run:
             for letter in letters:
                 x, y, ltr, visible = letter
                 if visible:
-                    dis = math.sqrt((x - m_x)**2 + (y - m_y)**2)
+                    dis = math.sqrt((x - m_x) ** 2 + (y - m_y) ** 2)
                     if dis < RADIUS:
                         letter[3] = False
+                        guessed.append(ltr)
+                        if ltr not in word:
+                            hangman_status += 1
+
+    # winning status
+    won = True
+    for letter in word:
+        if letter not in guessed:
+            won = False
+            break
+
+    if won:
+        win.fill(WHITE)
+        display_message("YOU WIN")
+        break
+
+    if hangman_status == 6:
+        display_message("YOU LOSE")
+        break
 
 pygame.quit()
